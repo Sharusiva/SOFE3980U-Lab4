5bc91c5e583f70f1df208012747c060e9dcea8f0
Sharusiva <sharujan.sivanandam@ontariotechu.net>
2026-03-16 01:32:39 -0400

    Updated files

--- MCC/src/main/java/com/ontariotechu/sofe3980U/App.java
+++ MCC/src/main/java/com/ontariotechu/sofe3980U/App.java
@@ -3,7 +3,9 @@ package com.ontariotechu.sofe3980U;
 
 import java.io.FileReader; 
 import java.util.List;
-import com.opencsv.*;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 
 /**
  * Evaluate Single Variable Continuous Regression
@@ -11,21 +13,21 @@ import com.opencsv.*;
  */
 public class App 
 {
-	private static final double EPSILON = 1e-15;
+	private static final float EPSILON = 1e-15f;
 
-	private static double clipProbability(double probability) {
+	private static float clipProbability(float probability) {
 		if (probability < EPSILON) {
 			return EPSILON;
 		}
-		if (probability > 1.0 - EPSILON) {
-			return 1.0 - EPSILON;
+		if (probability > 1.0f - EPSILON) {
+			return 1.0f - EPSILON;
 		}
 		return probability;
 	}
 
-	private static int argmax(double[] values) {
+	private static int argmax(float[] values) {
 		int bestIndex = 0;
-		double bestValue = values[0];
+		float bestValue = values[0];
 		for (int i = 1; i < values.length; i++) {
 			if (values[i] > bestValue) {
 				bestValue = values[i];
@@ -37,17 +39,17 @@ public class App
 
 	private static void printConfusionMatrix(int[][] matrix) {
 		int classes = matrix.length;
-		System.out.println("Confusion Matrix (rows=true class, cols=predicted class):");
-		System.out.print("\t");
+		System.out.println("Confusion matrix");
+		System.out.print("\t\t");
 		for (int col = 0; col < classes; col++) {
-			System.out.print((col + 1) + "\t");
+			System.out.print("y=" + (col + 1) + "\t");
 		}
 		System.out.println();
 
-		for (int row = 0; row < classes; row++) {
-			System.out.print((row + 1) + "\t");
-			for (int col = 0; col < classes; col++) {
-				System.out.print(matrix[row][col] + "\t");
+		for (int predicted = 0; predicted < classes; predicted++) {
+			System.out.print("\ty^=" + (predicted + 1) + "\t");
+			for (int trueClass = 0; trueClass < classes; trueClass++) {
+				System.out.print(matrix[trueClass][predicted] + "\t");
 			}
 			System.out.println();
 		}
@@ -75,24 +77,24 @@ public class App
 
 		int numberOfClasses = allData.get(0).length - 1;
 		int[][] confusionMatrix = new int[numberOfClasses][numberOfClasses];
-		double ceSum = 0.0;
+		float ceSum = 0.0f;
 
 		for (String[] row : allData) {
 			int yTrue = Integer.parseInt(row[0]);
-			double[] yPredicted = new double[numberOfClasses];
+			float[] yPredicted = new float[numberOfClasses];
 			for (int i = 0; i < numberOfClasses; i++) {
-				yPredicted[i] = Double.parseDouble(row[i + 1]);
+				yPredicted[i] = Float.parseFloat(row[i + 1]);
 			}
 
-			double probabilityOfTrueClass = clipProbability(yPredicted[yTrue - 1]);
-			ceSum += -Math.log(probabilityOfTrueClass);
+			float probabilityOfTrueClass = clipProbability(yPredicted[yTrue - 1]);
+			ceSum += -(float) Math.log(probabilityOfTrueClass);
 
 			int predictedClass = argmax(yPredicted) + 1;
 			confusionMatrix[yTrue - 1][predictedClass - 1]++;
 		}
 
-		double ce = ceSum / allData.size();
-		System.out.printf("Cross-Entropy (CE): %.6f%n", ce);
+		float ce = ceSum / allData.size();
+		System.out.println("CE =" + ce);
 		printConfusionMatrix(confusionMatrix);
 
 	}
